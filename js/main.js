fdc0665ed556fb9ee3c0aa86f259a285769214de
Pavel <pavelnerd@gmail.com>
2017-07-03 16:46:36 +0300

    add modal & minor changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
-// validate forms
+
+// $(function () {
+
+//   // validate forms
+//   $('#demo-form').parsley().on('field:validated', function() {
+//     var ok = $('.parsley-error').length === 0;
+//     // $('.bs-callout-info').toggleClass('hidden', !ok);
+//     // $('.bs-callout-warning').toggleClass('hidden', ok);
+//   })
+//   // .on('form:submit', function() {
+//   //   return false; // Don't submit form for this demo
+//   // });
+
+
+//   // show/hide form
+
+
+// });
+
+
+
+$(document).ready(function() {
+  $.viewInput = {
+    '0' : $([]),
+  //Это имя DIV вокруг невидимого поля
+    'otherField' : $('#otherField'),
+  };
+
+$('#otherFieldOption').change(function() {
+    // Прячет это поле, если выбрана другая опция
+    $.each($.viewInput, function() { this.hide(); this.find('textarea').attr('required', false); });
+  // Показывает поле при выборе необходимой опции
+    $.viewInput[$(this).val()].show().find('textarea').attr('required', true);
+  });
+
+});
+
+
 $(function () {
+
+  // validate forms
   $('#demo-form').parsley().on('field:validated', function() {
     var ok = $('.parsley-error').length === 0;
     // $('.bs-callout-info').toggleClass('hidden', !ok);
@@ -8,4 +47,9 @@ $(function () {
   // .on('form:submit', function() {
   //   return false; // Don't submit form for this demo
   // });
+
+
+  // show/hide form
+
+
 });
